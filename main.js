8eed92a2bf8b751e48564a891abf6a4f710497e2
wentingliu <cindy810317@gmail.com>
2021-10-26 11:16:15 +0800

    feat: get the checked value of drink options

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// new the alphaPos Instance
+const alphaPos = new AlphaPos()
+
+const addDrinkButton = document.querySelector('[data-alpha-pos="add-drink"]')
+addDrinkButton.addEventListener('click', function () {
+  const drinkName = alphaPos.getCheckedValue('drink')
+  const ice = alphaPos.getCheckedValue('ice')
+  const sugar = alphaPos.getCheckedValue('sugar')
+  console.log(`${drinkName}, ${ice}, ${sugar}`)
+})
+
+// Constructor function for Alpha Pos System
+function AlphaPos () { }
+AlphaPos.prototype.getCheckedValue = function (inputName) {
+  let selectedOption = ''
+  document.querySelectorAll(`[name=${inputName}]`).forEach(function (item) {
+    if (item.checked) {
+      selectedOption = item.value
+    }
+  })
+  return selectedOption
+}
+
 // Constructor function for Drinks
 function Drink (name, sugar, ice) {
   this.name = name
